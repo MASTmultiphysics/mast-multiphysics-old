70e54c9ef0f2262304ec33e39752dedc919714f3
Manav Bhatia <bhatiam@falcon.local>
2013-04-23 17:37:05 -0400

    Added transient solver theta as input to the driver.

--- FESystem/applications/euler/run_euler.cpp
+++ FESystem/applications/euler/run_euler.cpp
@@ -446,6 +446,7 @@ int main (int argc, char* const argv[])
     
     
     TwostepTimeSolver *timesolver = new TwostepTimeSolver(system);
+    EulerSolver *core_time_solver = new EulerSolver(system);
     
     //    timesolver->target_tolerance = infile("timesolver_tolerance",);
     //    timesolver->upper_tolerance  = infile("timesolver_upper_tolerance",);
@@ -455,8 +456,11 @@ int main (int argc, char* const argv[])
     timesolver->global_tolerance = infile("timesolver_global_tolerance", false);
     timesolver->max_growth       = infile("timesolver_maxgrowth", 1.5);
     timesolver->max_deltat       = infile("timesolver_max_deltat", 5.0e2);
-    timesolver->core_time_solver = AutoPtr<EulerSolver>(new EulerSolver(system));
     timesolver->reduce_deltat_on_diffsolver_failure = infile("timesolver_reduce_deltat_on_diffsolver_failure", 8);
+
+    core_time_solver->theta      = infile("timesolver_theta", 1.0);
+    
+    timesolver->core_time_solver = AutoPtr<UnsteadySolver>(core_time_solver);
     system.time_solver = AutoPtr<UnsteadySolver>(timesolver);
     
     //system.time_solver = AutoPtr<UnsteadySolver>(new EulerSolver(system));
