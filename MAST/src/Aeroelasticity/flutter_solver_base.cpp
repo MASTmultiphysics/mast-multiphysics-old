29d7f794af6e1ad37f0bf817a99abc374caaa8ed
Manav Bhatia <bhatiam@falcon.rb.rad-e.wpafb.af.mil>
2013-07-26 11:52:10 -0400

    minor changes

---
+++ MAST/src/Aeroelasticity/flutter_solver_base.cpp
@@ -0,0 +1,9 @@
+//
+//  flutter_solver_base.cpp
+//  RealSolver
+//
+//  Created by Manav Bhatia on 7/25/13.
+//  Copyright (c) 2013 Manav Bhatia. All rights reserved.
+//
+
+#include "flutter_solver_base.h"
---
+++ MAST/src/Aeroelasticity/flutter_solver_base.h
@@ -0,0 +1,14 @@
+//
+//  flutter_solver_base.h
+//  RealSolver
+//
+//  Created by Manav Bhatia on 7/25/13.
+//  Copyright (c) 2013 Manav Bhatia. All rights reserved.
+//
+
+#ifndef __RealSolver__flutter_solver_base__
+#define __RealSolver__flutter_solver_base__
+
+#include <iostream>
+
+#endif /* defined(__RealSolver__flutter_solver_base__) */
