c6e4bd75d01a2869725bb7fcf28320e5ca06084a
Manav Bhatia <bhatiamanav@gmail.com>
2014-07-11 17:18:53 -0400

    added structural model to Xcode.

--- MAST/src/Aeroelasticity/structural_model.h
+++ MAST/src/Aeroelasticity/structural_model.h
@@ -12,6 +12,9 @@
 // MAST includes
 #include "Base/MAST_data_types.h"
 
+// libMesh includes
+#include "libmesh/parameter_vector.h"
+
 
 class StructuralModel
 {
@@ -27,7 +30,17 @@ public:
      *    Returns false if the matrix does not exist for this model
      */
     virtual bool get_mass_matrix(RealMatrixX& m) = 0;
-    
+
+    /*!
+     *    updates the matrix to the sensitivity of mass matrix for
+     *    the structural model.
+     *    Returns false if the matrix does not exist for this model
+     */
+    virtual bool
+    get_mass_matrix_sensitivity(const libMesh::ParameterVector& params,
+                                unsigned int p,
+                                RealMatrixX& m) = 0;
+
     /*!
      *    updates the matrix to the stiffness matrix for this structural model
      *    Returns false if the matrix does not exist for this model
@@ -35,11 +48,31 @@ public:
     virtual bool get_stiffness_matrix(RealMatrixX& k) = 0;
     
     /*!
+     *    updates the matrix to the sensitivity of stiffness matrix for
+     *    the structural model.
+     *    Returns false if the matrix does not exist for this model
+     */
+    virtual bool
+    get_stiffness_matrix_sensitivity(const libMesh::ParameterVector& params,
+                                     unsigned int p,
+                                     RealMatrixX& m) = 0;
+
+    /*!
      *    updates the matrix to the damping matrix for this structural model
      *    Returns false if the matrix does not exist for this model
      */
     virtual bool get_damping_matrix(RealMatrixX& c) = 0;
     
+    /*!
+     *    updates the matrix to the sensitivity of damping matrix for
+     *    the structural model.
+     *    Returns false if the matrix does not exist for this model
+     */
+    virtual bool
+    get_damping_matrix_sensitivity(const libMesh::ParameterVector& params,
+                                   unsigned int p,
+                                   RealMatrixX& m) = 0;
+
 };
 
 
